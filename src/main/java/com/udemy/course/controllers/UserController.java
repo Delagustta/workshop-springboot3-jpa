e3f45f5de13e5c0591a97566bbee32cefd808a91
Gustavo Funchal <delagusttaf@gmail.com>
2023-10-16 23:19:21 -0300

    Added user entity and controller

---
+++ src/main/java/com/udemy/course/controllers/UserController.java
@@ -0,0 +1,18 @@
+package com.udemy.course.controllers;
+
+import com.udemy.course.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User user = new User(1L, "Maria", "maria@maria.com", "999999", "123456");
+        return ResponseEntity.ok().body(user);
+    }
+}
