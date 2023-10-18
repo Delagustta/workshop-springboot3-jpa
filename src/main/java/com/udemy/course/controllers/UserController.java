3d673f5f2afff6360d1c0b94d3045cbc2443631b
Gustavo Funchal <delagusttaf@gmail.com>
2023-10-17 22:33:05 -0300

    UserService and new UserController methods

--- src/main/java/com/udemy/course/controllers/UserController.java
+++ src/main/java/com/udemy/course/controllers/UserController.java
@@ -1,18 +1,34 @@
 package com.udemy.course.controllers;
 
 import com.udemy.course.entities.User;
+import com.udemy.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
 
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Maria", "maria@maria.com", "999999", "123456");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll() {
+        return ResponseEntity.ok().body(userService.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(userService.findById(id));
     }
 }
